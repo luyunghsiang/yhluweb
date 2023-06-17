fc93a6db0566d42a6a83ceb3477fe320627318c4
luyunghsiang <luyunghsiang@users.noreply.github.com>
2023-06-17 18:41:14 +0000

    deploy: 4370a5e3704c200e898694bb86e401b51df68544

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'Updated 2023-03-24',
+    VERSION: 'Updated 2023-06-17',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
